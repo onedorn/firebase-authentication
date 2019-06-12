22695bb1ba7086677af321c501a831c18858c44e
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-06-13 00:35:13 +0300

    Create guides and record to the database.

--- scripts/auth.js
+++ scripts/auth.js
@@ -9,6 +9,23 @@ auth.onAuthStateChanged(user => {
         setupUi();
         setUpGuides([]);        
     }
+});
+
+// Create new guide
+const createForm = document.querySelector('#create-form');
+
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value
+    }).then(() => {
+        // close modal and reset form
+        const modal = document.querySelector('#modal-create');
+        M. Modal.getInstance(modal).close();
+        createForm.reset();
+    })
 })
 
 
